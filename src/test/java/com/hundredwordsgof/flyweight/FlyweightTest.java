6ca06a15b6e1f879de1c6b2ff0c4bb40cc5d248d
JonathanPugh <35753762+JonathanPugh@users.noreply.github.com>
2018-03-28 17:57:05 -0500

    Add failing tests to FlyweightTest.java

--- src/test/java/com/hundredwordsgof/flyweight/FlyweightTest.java
+++ src/test/java/com/hundredwordsgof/flyweight/FlyweightTest.java
@@ -66,4 +66,23 @@ public class FlyweightTest {
               .getState());
     }
   }
+  
+    @Test
+  public void testFlyweight() {
+
+    Flyweight flyweight1 = FlyweightFactory.getFlyweight("key1", "value1");
+    Flyweight flyweight2 = FlyweightFactory.getFlyweight("key2", "value2");
+    Flyweight flyweight3 = FlyweightFactory.getFlyweight("key1", "value3");
+
+    Flyweight unsharedFlyweight1 = FlyweightFactory.getFlyweight("unsharedKey1",
+        "value1");
+    Flyweight unsharedFlyweight2 = FlyweightFactory.getFlyweight("unsharedKey2",
+        "value2");
+    Flyweight unsharedFlyweight3 = FlyweightFactory.getFlyweight("unsharedKey1",
+        "value3");
+
+    assertEquals(flyweight1, flyweight2);
+    assertNotEquals(flyweight1, flyweight3);
+    assertEquals(flyweight2, flyweight3);
+  }
 }
